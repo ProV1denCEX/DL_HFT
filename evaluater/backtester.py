1b0f1c2eb3a41a9c47949394de9f3d147aaabd0b
Frontal Xiang <firstematxcx@gmail.com>
2020-05-05 06:08:55 -0400

    add backtest

--- evaluater/backtester.py
+++ evaluater/backtester.py
@@ -1,14 +1,52 @@
+import h5py
+import numpy as np
+from evaluater.signal_generator import SignalGenerator
+
+
 class Backtester(object):
     def __init__(self):
-        pass
+        self.data = None
+        self.nv = None
 
-    def run(self):
-        pass
+    def run(self, signal):
+        ret = np.diff(self.data, axis=0)
+        ret = ret * signal
+        self.ret = ret
+        ret = ret.sum(axis=1) + 1
+
+        nv = ret.cumprod()
+        self.nv = nv
 
     def evaluate(self):
-        pass
+        win_rate = sum(np.diff(self.nv) > 0) / len(self.nv)
+        ret = self.nv[-1] - 1
+        sharpe = ret / self.ret.std()
+
+        print("win_rate:", win_rate)
+        print("return in backtest:", ret)
+        print("sharpe:", sharpe)
+
+    def load_original_data(self):
+        with h5py.File("data/TSLA0107to0108.h5", "r") as org:
+            # List all groups
+            a_group_key = list(org.keys())[0]
+            raw_data = list(org[a_group_key].items())
+            bytes_columns = raw_data[0][1][()]
+            columns = [i.decode('utf-8') for i in bytes_columns]
+            data_1 = raw_data[-1][1][()]
+
+            self.data = data_1[151000:200000, [0, 2]]
 
 
 if __name__ == '__main__':
-    pass
+    bt = Backtester()
+    bt.load_original_data()
+
+    signal = SignalGenerator()
+    signal.load_prediction()
+    signal.generate_signal()
+
+    bt.run(signal.signal)
+
+    bt.evaluate()
 
--- evaluater/signal_generator.py
+++ evaluater/signal_generator.py
@@ -1,35 +1,32 @@
 import h5py
-import pandas as pd
+import numpy as np
+
 
 class SignalGenerator(object):
     def __init__(self):
-        pass
+        self.pred_data = None
+        self.signal = None
+
+    def load_prediction(self):
+        prediction = "evaluater/predictions.hdf5"
+
+        with h5py.File(prediction, "r") as pred:
+            # List all groups
+            a_group_key = list(pred.keys())[0]
+
+            # Get the data
+            self.pred_data = np.vstack(list(pred[a_group_key]))
 
     def generate_signal(self):
-        pass
+        self.signal = np.zeros([len(self.pred_data), 2])
+
+        self.signal[self.pred_data[:, 0] < 0, 0] = -1
+        self.signal[self.pred_data[:, 1] < 0, 1] = -1
+        self.signal[self.pred_data[:, 0] > 0, 0] = 1
+        self.signal[self.pred_data[:, 1] > 0, 1] = 1
 
 
 if __name__ == '__main__':
-    prediction = "evaluater/predictions.hdf5"
+    pass
 
-    with h5py.File(prediction, "r") as pred:
-        # List all groups
-        print("Keys: %s" % pred.keys())
-        a_group_key = list(pred.keys())[0]
-
-        # Get the data
-        data = list(pred[a_group_key])
-
-        print(1)
-
-    # test = pd.read_hdf("data/TSLA0107to0108.h5")
-    with h5py.File("data/TSLA0107to0108.h5", "r") as org:
-        # List all groups
-        print("Keys: %s" % org.keys())
-        b_group_key = list(org.keys())[0]
-
-        # Get the data
-        test = list(org[b_group_key])
-
-        print(1)
 
